41d9c7fc998368063b02f2f0bf84e39f04fa8067
Yahya Mohamed <yah.mohamed03@gmail.com>
2026-04-19 17:36:01 +0100

    Add a redirect route from /auth to /auth/signin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import AuthLayout from "./pages/auth.tsx";
 import {SignUp,LogIn} from "./pages/auth.tsx";
 import Dashboard from "./pages/dashboard.tsx";
@@ -9,6 +9,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<AuthLayout/>}>
+          <Route index element={<Navigate to="signin"/>}/>
           <Route path="signin" element={<LogIn/>}/>
           <Route path="signup" element={<SignUp/>}/>
         </Route>  
